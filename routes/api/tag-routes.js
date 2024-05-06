459c1d1d15d131fad3d61cae833477c3938eafe2
Tania <tanner.shirkey@gmail.com>
2024-05-06 18:22:38 -0400

    Defined tag route data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,81 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get("/", (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag was found with that id" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  // create a new tag
+router.post("/", async (req, res) => {
+  try {
+    const tagData = await Category.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const tagData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag was found with that id" });
+      return;
+    }
+
+    const updatedData = await tagData.update({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(updatedData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag was found with that id" });
+      return;
+    }
+
+    tagData.destroy();
+    res.status(200).json("Tag deleted successfully!");
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
